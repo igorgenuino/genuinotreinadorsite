2e35ae6b3fe8a0daa8b93c3c8b9f48387f90931e
igorgenuino <igor.margen@gmail.com>
2020-07-28 21:12:55 -0300

    Segunda aula da imersao

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from '../../assets/images/logo.png';
 import Button from '../Button'
 import './Menu.css';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Genuino Treinado Logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video/">
                 Novo Vídeo
             </Button>
         </nav>
